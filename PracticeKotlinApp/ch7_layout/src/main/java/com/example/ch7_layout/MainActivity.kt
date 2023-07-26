2556ce735119814ef1cfd19517df9f5be0e4bff8
parkUiery <parkuiery@gmail.com>
2023-07-26 14:06:31 +0900

    파일 빌드

--- PracticeKotlinApp/ch7_layout/src/main/java/com/example/ch7_layout/MainActivity.kt
+++ PracticeKotlinApp/ch7_layout/src/main/java/com/example/ch7_layout/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.ch7_layout
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+//ConstLayout
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
