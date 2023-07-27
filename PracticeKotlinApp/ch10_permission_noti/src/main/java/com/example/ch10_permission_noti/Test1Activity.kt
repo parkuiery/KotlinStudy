d69e5ff5fad02249f5dc0c34231c5389942741b5
parkUiery <parkuiery@gmail.com>
2023-07-27 11:29:55 +0900

    파일 빌드

---
+++ PracticeKotlinApp/ch10_permission_noti/src/main/java/com/example/ch10_permission_noti/Test1Activity.kt
@@ -0,0 +1,11 @@
+package com.example.ch10_permission_noti
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Test1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test1)
+    }
+}
