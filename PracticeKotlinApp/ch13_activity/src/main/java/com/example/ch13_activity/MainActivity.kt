0120e86f9895de9b5e9bb151dcf99380f120bca4
parkUiery <parkuiery@gmail.com>
2023-07-27 15:24:32 +0900

    파일 빌드

--- PracticeKotlinApp/ch13_activity/src/main/java/com/example/ch13_activity/MainActivity.kt
+++ PracticeKotlinApp/ch13_activity/src/main/java/com/example/ch13_activity/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.ch13_activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
