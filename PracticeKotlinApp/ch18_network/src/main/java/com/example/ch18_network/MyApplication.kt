1d4328d60db0007b989db7f1702488e1e4515e1e
parkUiery <parkuiery@gmail.com>
2023-07-28 23:30:51 +0900

    api 통신

--- PracticeKotlinApp/ch18_network/src/main/java/com/example/ch18_network/MyApplication.kt
+++ PracticeKotlinApp/ch18_network/src/main/java/com/example/ch18_network/MyApplication.kt
@@ -1,4 +1,22 @@
 package com.example.ch18_network
 
-class MyApplication {
+import android.app.Application
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+//retrofit 객체를 앱이 실행되면서 최초에 한번 초기화 해야 한다
+//여러 곳에서 할수도 있지만 Application 객체에서 하는게 가장 좋다
+//Application - 앱의 프로세스가 살면서 가장 처음 실행시켜주는 객체, 한번만 실행
+class MyApplication :Application(){
+    var networkService: InetworkService
+
+    val retrofit: Retrofit
+        get() = Retrofit.Builder()
+            .baseUrl("https://regres.in/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    init {
+        //우리가 만든 인터페이스 등록
+        networkService = retrofit.create(InetworkService::class.java)
+    }
 }
