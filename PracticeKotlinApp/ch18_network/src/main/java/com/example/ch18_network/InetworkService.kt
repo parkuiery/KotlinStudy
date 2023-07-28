94ce9b99f6467a485e1ed5abc65ccd12537a7e54
parkUiery <parkuiery@gmail.com>
2023-07-28 23:28:44 +0900

    api 통신

--- PracticeKotlinApp/ch18_network/src/main/java/com/example/ch18_network/InetworkService.kt
+++ PracticeKotlinApp/ch18_network/src/main/java/com/example/ch18_network/InetworkService.kt
@@ -1,4 +1,16 @@
 package com.example.ch18_network
 
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.http.Url
+
 interface InetworkService {
+    //서버 연동시 호출할 함수를 정의, annotation으로 정보 지정하면서
+    @GET("api/users")
+    fun getUserList(@Query("page")page: String): Call<UserList>
+
+    @GET
+    fun getAvatarImage(@Url url: String): Call<ResponseBody>
 }
